1b194efcdf519f966c01fd60b4bcf6cca20e7a08
Santiago Gallego <santiago.gallego.rodrigo@gmail.com>
2017-12-09 17:01:49 +0100

    Refactor and added Bluetooth service

--- app/src/main/java/com/example/egalsan/u8g2droid/U8gMainActivity.kt
+++ app/src/main/java/com/example/egalsan/u8g2droid/U8gMainActivity.kt
@@ -9,17 +9,24 @@ import android.widget.Toast
 import android.util.Log
 import android.content.DialogInterface
 import android.support.v7.app.AlertDialog
+import android.bluetooth.BluetoothSocket
+import java.io.IOException
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class U8gMainActivity : AppCompatActivity() {
 
     companion object {
-        const val LOG_TAG: String = "MainActivity"
+        const val LOG_TAG: String = "U8gMainActivity"
         const val REQUEST_ENABLE_BT: Int = 1337
         const val PREFS_NAME = "u8g2droid"
+        //TODO: const val MY_UUID = "093380fb-fc88-4b0c-930c-d1c10947ec1e"
+        //TODO: Maybe use 00001101-0000-1000-8000-00805F9B34FB (from createRfcommSocketToServiceRecord() documentation)
+        const val MY_UUID = "00001101-0000-1000-8000-00805F9B34FB"
     }
 
     private val mBluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private val mUuid: UUID = UUID.fromString(MY_UUID)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,17 +34,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Make sure Bluetooth is available
-        assertBluetoothIsAvailable()
+        if (! assertBluetoothIsAvailable()) {
+            Toast.makeText(this, "The device does not support Bluetooth, exiting...", Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
+        startBT()
+    }
+
+
+    private fun startBT() {
         // Read stored settings
         val settings = getSharedPreferences(PREFS_NAME, 0)
         val deviceName = settings.getString("deviceName", null)
         val deviceMac = settings.getString("deviceMac", null)
 
         if (deviceName == null || deviceMac == null) {
+            Log.v(LOG_TAG, "No previous device found, selecting a Bluetooth device to connect to")
             selectBluetoothDevice()
         } else {
             // Start Bluetooth connection, trying to reconnect to the stored device
+            Log.v(LOG_TAG, "Previous device found, trying to reconnect to Bluetooth device")
             startBluetoothConnection(deviceName, deviceMac)
         }
     }
@@ -49,14 +66,11 @@ class MainActivity : AppCompatActivity() {
         // This is executed when Bluetooth is enabled
         if (requestCode == REQUEST_ENABLE_BT) {
             if (resultCode == RESULT_OK) {
+                Log.v(LOG_TAG, "Bluetooth enabled successfully")
                 Toast.makeText(this, "Bluetooth enabled successfully", Toast.LENGTH_SHORT).show()
-                val deviceName = data?.extras?.getString("deviceName")
-                val deviceMac = data?.extras?.getString("deviceMac")
-
-                // Start connection
-                startBluetoothConnection(deviceName, deviceMac)
-
+                startBT()
             } else if (resultCode == RESULT_CANCELED) {
+                Log.e(LOG_TAG, "Error when trying to enable Bluetooth")
                 Toast.makeText(this, "Error when trying to enable Bluetooth", Toast.LENGTH_SHORT).show()
                 finish()
             }
@@ -67,12 +81,8 @@ class MainActivity : AppCompatActivity() {
     /**
      * Make sure that Bluetooth is available
      */
-    private fun assertBluetoothIsAvailable() {
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            Toast.makeText(this, "The device does not support Bluetooth, exiting...", Toast.LENGTH_SHORT).show()
-            finish()
-        }
+    private fun assertBluetoothIsAvailable(): Boolean {
+        return mBluetoothAdapter != null
     }
 
     /**
@@ -83,19 +93,22 @@ class MainActivity : AppCompatActivity() {
 
         // Make sure Bluetooth is enabled
         if (! mBluetoothAdapter!!.isEnabled) {
+            Log.i(LOG_TAG, "Bluetooth is not enabled, trying to enable it")
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            enableBtIntent.putExtra("deviceName", deviceName)
-            enableBtIntent.putExtra("deviceMac", deviceMac)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         } else {
-            // TODO: Start connection as client
+            Log.i(LOG_TAG, "Bluetooth is enabled")
+            // Start connection as client
+            val bondedDevices = mBluetoothAdapter!!.bondedDevices
+            val btDevice = getBluetoothDevice(deviceName, bondedDevices)
+            val btThread = U8gConnectThread(btDevice!!)
+            btThread.start()
 
+            Log.i(LOG_TAG, "Trying to setup Bluetooth connection with '" + deviceName + "' at " + deviceMac)
             Toast.makeText(
                     this,
-                    "TODO: Trying to setup Bluetooth connection with '" + deviceName + "' at " + deviceMac,
+                    "Trying to setup Bluetooth connection with '" + deviceName + "' at " + deviceMac,
                     Toast.LENGTH_SHORT).show()
-
-            // TODO
         }
     }
 
@@ -150,7 +163,9 @@ class MainActivity : AppCompatActivity() {
             val editor = settings.edit()
             editor.putString("deviceName", device?.name)
             editor.putString("deviceMac", device?.address)
-            editor.commit()
+            editor.apply()
+
+            Log.i(LOG_TAG, "Stored Bluetooth device '" + device?.name + "' at " + device?.address)
 
             // Start connection
             startBluetoothConnection(device?.name, device?.address)
@@ -158,4 +173,59 @@ class MainActivity : AppCompatActivity() {
         builder.show()
     }
 
+    private inner class U8gConnectThread(private val mmDevice: BluetoothDevice) : Thread() {
+        private val mmSocket: BluetoothSocket?
+
+        init {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            var tmp: BluetoothSocket? = null
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                tmp = mmDevice.createRfcommSocketToServiceRecord(mUuid)
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "Socket's create() method failed", e)
+            }
+
+            mmSocket = tmp
+        }
+
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            mBluetoothAdapter!!.cancelDiscovery()
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket!!.connect()
+            } catch (connectException: IOException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    Log.e(LOG_TAG, "Unable to connect with Bluetooth device '" + mmDevice.name + "' at " + mmDevice.address)
+                    mmSocket!!.close()
+                } catch (closeException: IOException) {
+                    Log.e(LOG_TAG, "Could not close the client socket", closeException)
+                }
+
+                return
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            // TODO: manageMyConnectedSocket(mmSocket)
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                Log.v(LOG_TAG, "Closing connection with Bluetooth device '" + mmDevice.name + "' at " + mmDevice.address)
+                mmSocket!!.close()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "Could not close the client socket", e)
+            }
+
+        }
+    }
 }
